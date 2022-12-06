daa7f1a0a0ae78cf39d23329d89e2a89ce654ecc
nhan <nhanng19@gmail.com>
2022-12-06 03:50:34 -0800

    routes to create posts

--- controllers/api/blogpostRoutes.js
+++ controllers/api/blogpostRoutes.js
@@ -19,7 +19,7 @@ router.get("/", async (req, res) => {
       logged_in: req.session.logged_in,
     });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
 });
 
